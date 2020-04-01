52d41918c4383eac7a70cc11deb98133dbbe9bba
ReD <vvassilyev@gmail.com>
2020-04-01 21:27:59 +0300

    результат занятия по исправлению клиента

--- client.cpp
+++ client.cpp
@@ -5,9 +5,19 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <cstring>
+#include <cstdio>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
 #define SERVER_PORT 12345 /* По договоренности между клиентом и сервером */
 #define BUF_SIZE 4096 /* Размер передаваемых блоков */
-int main(int argc, char *argv)
+
+void fatal(const char *string);
+
+int main(int argc, char *argv[])
 {
     int c,s,bytes;
     char buf[BUF_SIZE]; /*буфер для входящего файла */
@@ -15,17 +25,17 @@ int main(int argc, char *argv)
     struct sockaddr_in channel; /*хранит IP=адрес */
     if (argc!=3) fatal("Для запуска введите: client имя_сервера имя_файла");
     h = gethostbyname(argv[1]); /* поиск IP-адреса хоста */
-    if(!h) fatal("Ошибка выполнения gethostbyname")
+    if(!h) fatal("Ошибка выполнения gethostbyname");
     s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (s<0) fatal("Сокет");
     memset(&channel, 0, sizeof(channel));
     channel.sin_family=AF_INET;
-    memcpy(&channel.sin_addr.s_addr,h>h_addr,h>h_length);
+    memcpy(&channel.sin_addr.s_addr,h->h_addr,h->h_length);
     channel.sin_port=htons(SERVER_PORT);
     c = connect(s,(struct sockaddr *) &channel, sizeof(channel));
     if (c<0) fatal("Ошибка соединения");
     /* Соединение установлено. Посылается имя файла с нулевым байтом на конце */
-    write*s, argv[2], strlen(argv[2])+1);
+    write (s, argv[2], strlen(argv[2])+1);
     /* Получить файл, записать на стандартное устройство вывода */
     while (1) {
         bytes = read(s, buf, BUF_SIZE); /* Читать из сокета */
@@ -33,7 +43,7 @@ int main(int argc, char *argv)
         write(1, buf, bytes); /* Записать на стандартное устройство вывода */
     }
 }
-fatal(char *string)
+void fatal(const char *string)
 {
     printf("%s\n", string);
     exit(1);
